075ce4df95b6169005d00d4ac301cdc066c4f9c7
NurkaAmre <nurkerey30@gmail.com>
2024-08-13 12:38:42 +0600

    Add module css and style

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AppNav() {
+  return (
+    <nav>
+      App Navigation
+    </nav>
+  )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import PageNav from "../components/PageNav"
-
+import { NavLink } from "react-router-dom"
 export default function HomePage() {
   return (
     <div>
       <PageNav />
-      World Wise
+      <h1> World Wise</h1>
+      <NavLink to="/app">Go to the App</NavLink>
     </div>
   )
 }
