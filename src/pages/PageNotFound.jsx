338599cf6b79d57d6f4e614a5d9852a28e4af5f1
NurkaAmre <nurkerey30@gmail.com>
2024-08-13 14:54:29 +0600

    Implement homepage

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,7 @@
-import PageNav from "../components/PageNav"
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      PageNotFound
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
