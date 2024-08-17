099244534181145166a0594128a8b54dece3546c
NurkaAmre <nurkerey30@gmail.com>
2024-08-17 17:13:03 +0600

    Finishing the city view

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,7 @@
 import { useSearchParams, useParams } from "react-router-dom";
 import styles from "./City.module.css";
+import { useCities } from "../contexts/CitiesContext";
+import { useEffect } from "react";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,61 +13,52 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-   const [searchParams, setSearchParams] = useSearchParams()
-  
-  const lat = searchParams.get('lat')
-  const lng = searchParams.get('lng')
+   const {getCity, currentCity} = useCities()
 
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+
+  useEffect(function () { 
+    getCity(id)
+  }, [id]);
+
 
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
-    <>
-      <h1>Map</h1>
-      <h1>Position: {lat}, {lng}</h1>
-    </>
-    // <div className={styles.city}>
-    //   <div className={styles.row}>
-    //     <h6>City name</h6>
-    //     <h3>
-    //       <span>{emoji}</span> {cityName}
-    //     </h3>
-    //   </div>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
 
-    //   <div className={styles.row}>
-    //     <h6>You went to {cityName} on</h6>
-    //     <p>{formatDate(date || null)}</p>
-    //   </div>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
 
-    //   {notes && (
-    //     <div className={styles.row}>
-    //       <h6>Your notes</h6>
-    //       <p>{notes}</p>
-    //     </div>
-    //   )}
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
 
-    //   <div className={styles.row}>
-    //     <h6>Learn more</h6>
-    //     <a
-    //       href={`https://en.wikipedia.org/wiki/${cityName}`}
-    //       target="_blank"
-    //       rel="noreferrer"
-    //     >
-    //       Check out {cityName} on Wikipedia &rarr;
-    //     </a>
-    //   </div>
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
 
-    //   {/* <div>
-    //     <ButtonBack />
-    //   </div> */}
-    // </div>
+      {/* <div>
+        <ButtonBack />
+      </div> */}
+    </div>
   );
 }
 
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -5,6 +5,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([])
   const [loading, setLoading] = useState(true)
+  const [currentCity, setCurrentCity] = useState({})
 
   const URL = "http://localhost:3001"
 
@@ -26,13 +27,32 @@ function CitiesProvider({ children }) {
     }
     fetchCities()
   }
-  , [])
+    , [])
+  
+  async function getCity(id) {
+      try {
+        setLoading(true)
+        const response = await fetch(`${URL}/cities/${id}`)
+        const data = await response.json()
+        console.log(data)
+        setCurrentCity(data)
+        setLoading(false)
+      } catch (error) {
+        console.error("Error fetching data", error)
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+
+
   return (
-    <CitiesContext.Provider value={{ cities, loading }}>
+    <CitiesContext.Provider value={{ cities, loading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   )
 }
+
 
 function useCities() {
   const context = useContext(CitiesContext)
